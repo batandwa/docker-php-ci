59698b1956c7e25a0423fb45dd850640d17a6d94
Batandwa Colani <i@batandwa.me>
2015-10-25 20:49:26 +0200

    Setup update.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install --yes curl
+chmod -Rf 0777 /var/app/PHPCI/build
 curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 apt-get install --yes nodejs
 apt-get install --yes build-essential
