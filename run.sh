9cd3936cf3c10e75ba5fec5c1ec75eeece9920ed
Batandwa Colani <i@batandwa.me>
2015-11-02 17:06:54 +0200

    Update run compose for dev.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#COMPOSE_FILE=${COMPOSE_FILE:?"docker-compose-dev.yml"}
+if [ -z "${COMPOSE_FILE}" ]; then export COMPOSE_FILE="docker-compose-dev.yml"; fi
 docker-compose up -d
 #docker cp `basename $PWD`:/etc/apache2/mods-available/rewrite.load server_conf/rewrite.load
 #docker cp `basename $PWD`:/etc/apache2/sites-available/000-default.conf server_conf/000-default.conf
