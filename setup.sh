dcf7e2caf21e2296b605f28a61f455ef68761630
Batandwa Colani <i@batandwa.me>
2015-11-09 18:38:08 +0200

    Touch placement.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 apt-get update
 apt-get install --yes curl
 chmod -Rf 0777 /var/app/PHPCI/build
@@ -6,6 +7,7 @@ curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 apt-get install --yes nodejs
 apt-get install --yes build-essential
 npm install -g grunt-cli
-touch /projects/.build-tool-set-up
 apt-get install --yes ruby
 gem install sass
+
+touch /var/.build-tool-set-up
