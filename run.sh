6993e3d6eee23ab6d3274d22530bcfcd7d7444e1
Batandwa Colani <i@batandwa.me>
2015-11-20 18:48:57 +0200

    Bundled database in project. Tweak start and setup scripts.

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #COMPOSE_FILE=${COMPOSE_FILE:?"docker-compose-dev.yml"}
-if [ -z "${COMPOSE_FILE}" ]; then export COMPOSE_FILE="docker-compose-dev.yml"; fi
+if [ -z "${COMPOSE_FILE}" ]; then export COMPOSE_FILE="docker-compose-prod.yml"; fi
+echo "Running $COMPOSE_FILE"
 docker-compose up -d
 #docker cp `basename $PWD`:/etc/apache2/mods-available/rewrite.load server_conf/rewrite.load
 #docker cp `basename $PWD`:/etc/apache2/sites-available/000-default.conf server_conf/000-default.conf
 #docker-compose up -d --force-recreate
-docker exec `basename $PWD` a2enmod rewrite
-docker restart `basename $PWD`
+#docker exec `basename $PWD` a2enmod rewrite
+#docker restart `basename $PWD`
 docker exec -it `basename $PWD` sh -c 'echo "*  *  *  *  *  /var/app/console phpci:run-builds" >> /etc/cron.d/phpci'
--- setup.sh
+++ setup.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-apt-get update
-apt-get install --yes curl
 chmod -Rf 0777 /var/app/PHPCI/build
-curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
-apt-get install --yes nodejs
-apt-get install --yes build-essential
-npm install -g grunt-cli
-apt-get install --yes ruby
-gem install sass
+dock='docker exec `basename $PWD`'
 
-touch /var/.build-tool-set-up
+# Install Grunt
+#docker exec `basename $PWD` apt-get update
+#docker exec `basename $PWD` apt-get install --yes curl
+#docker exec `basename $PWD` curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+#docker exec `basename $PWD` apt-get install --yes nodejs build-essential
+#docker exec `basename $PWD` npm install -g grunt-cli
+
+# Install SASS
+#docker exec `basename $PWD` apt-get install --yes ruby
+#docker exec `basename $PWD` gem install sass
+
+docker exec `basename $PWD`-db mkdir /var/log/mysql -p
+docker exec `basename $PWD`-db touch /var/log/mysql/mysql_queries.log
+docker exec `basename $PWD`-db chown mysql:mysql /var/log/mysql/ -Rf
+docker exec `basename $PWD` a2enmod rewrite
+docker exec `basename $PWD` sh -c 'echo "*  *  *  *  *  /var/app/console phpci:run-builds" >> /etc/cron.d/phpci'
+
+docker-compose restart
+
+docker exec `basename $PWD` mv /var/app/PHPCI/config.yml /var/app/PHPCI/config.yml.`date +%Y%m%d%H%M%S`
+#docker exec `basename $PWD` /var/app/console phpci:install --url=http://$VIRTUAL_HOST --db-host=db --db-name=phpci --db-user=root --db-pass=doinkydoinkwalking --admin-name=admin --admin-pass=doinkydoinkwalking --admin-mail=admin@batandwa.me --no-ansi --no-interaction
+
+#docker exec `basename $PWD` touch /var/.build-tool-set-up
+
+
+
+#SET global general_log_file='/var/log/mysql/mysql.log'; SET global general_log = on; SET global log_output = 'file'
