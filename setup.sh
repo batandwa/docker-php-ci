1c3b756cf7e7947bec03e2a393c1ae92b39d1d0b
Batandwa Colani <i@batandwa.me>
2015-11-21 15:08:44 +0200

    Added build status file.

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ cd app
 docker exec `basename $PWD` mv /var/app/PHPCI/config.yml /var/app/PHPCI/config.yml.`date +%Y%m%d%H%M%S`
 docker exec `basename $PWD` /var/app/console phpci:install --url=http://$VIRTUAL_HOST --db-host=db --db-name=phpci --db-user=root --db-pass=doinkydoinkwalking --admin-name=admin --admin-pass=doinkydoinkwalking --admin-mail=admin@batandwa.me --no-ansi --no-interaction
 
-#docker exec `basename $PWD` touch /var/.build-tool-set-up
+docker exec `basename $PWD` touch /var/.build-tool-set-up
 
 
 
