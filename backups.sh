6d331684fe7435fc2d7f69705645465a3054fe82
Batandwa Colani <i@batandwa.me>
2016-01-27 01:13:24 +0200

    Added db backups. Remove setup script.

---
+++ backups.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+HOST="localhost"
+
+#rm "$OUTPUTDIR/*gz" > /dev/null 2>&1
+
+databases=`mysql -u $MYSQL_USER -h $HOST -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+
+for db in $databases; do
+  if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+    echo "Dumping database: $db"
+    mysqldump -u $MYSQL_USER -h $HOST -p$MYSQL_PASSWORD --databases $db | gzip -9 > /backups/${db}_`date +%Y%m%d_%H%M%S`.sql.gz
+  fi
+done
